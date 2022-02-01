53cdb6d6100782381dad500da6a7999073fc345a
Ansh Bhardwaj <93650767+anshyo@users.noreply.github.com>
2022-02-01 17:28:06 +0530

    1st change

---
+++ tests/notification.py
@@ -0,0 +1,11 @@
+from plyer import *
+import time
+
+
+notification.notify(
+    title = "HEADING HERE",
+    message=" DESCRIPTION HERE" ,
+           
+    timeout=10
+)
+time.sleep(7)
---
+++ v0.py
@@ -0,0 +1,13 @@
+from json.tool import main
+import os
+import webbrowser
+import pyttsx3
+import datetime
+import speech_recognition as sp
+
+
+
+
+
+
+
